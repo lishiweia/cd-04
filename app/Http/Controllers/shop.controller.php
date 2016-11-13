fdb9ba69771489f71ef831f11c005a9a5ab0d144
lishiwei <710446319@qq.com>
2016-11-13 21:22:22 +0800

    111

--- app/Http/Controllers/shop.controller.php
+++ app/Http/Controllers/shop.controller.php
@@ -1,17 +1,5 @@
 <?php
 
-echo '购物车11';
-echo 'aaaa';
-echo 'aaaa';
-
-echo 'aaaa';
-echo 'aaaa';
-
-echo '我也写了一个';
-echo '购物车11222';
-
-echo '我也写了一个';
-echo '购物车11222';
-echo '我也写了一个';
-echo '购物车11222';
+echo 'asdasdas';
+echo 'bbbb';
 
